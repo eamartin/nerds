1e3303c72fc0705a312e49809bcfbac908880140
Eric Martin <e.a.martin1337@gmail.com>
2013-08-18 09:03:39 +0000

    fixed setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from distutils.core import setup
 from pip.req import parse_requirements
 
-reqs = map(str, parse_requirements("requirements.txt"))
+reqs = parse_requirements("requirements.txt")
+reqs = [str(ir.req) for ir in reqs]
 
 setup(
     name="Nerds4Life",
