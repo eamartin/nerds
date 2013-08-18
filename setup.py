2589dbbdd932a9d0eccae1a50eaeed437f8cf555
Eric Martin <e.a.martin1337@gmail.com>
2013-08-18 09:01:41 +0000

    Might work as package now

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from distutils.core import setup
+from pip.req import parse_requirements
+
+reqs = map(str, parse_requirements("requirements.txt"))
 
 setup(
     name="Nerds4Life",
     version="1.0",
-    packages=["nerds"]
+    packages=["nerds"],
+    install_requires=reqs
 )
